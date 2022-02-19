6e55e0a2f30a87725d4a1e6af9cc9696c7342a57
masanori iha <masanori.iha@gmail.com>
2022-02-19 15:25:38 -0300

    sequencia do Dashboard com a criação da tabela de informações

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
 import { Container } from "./style";
+import { TansactionTable } from "../TransactionTable";
 
 export function Dashboard(){
       return (
             <Container>
                   <Summary/>
+                  <TansactionTable />
             </Container>
       )
 }
