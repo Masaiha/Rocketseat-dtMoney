391b55f56305eafceb80627a228641592933e23a
masanori iha <masanori.iha@gmail.com>
2022-02-19 15:59:13 -0300

    instalando e configurando api fake - com miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes()  {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Tansaction 1',
+          amount: 400,
+          category: 'Food',
+          createdAt: new Date()
+        },
+        {
+          id: 2,
+          title: 'Tansaction 2',
+          amount: 200,
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
