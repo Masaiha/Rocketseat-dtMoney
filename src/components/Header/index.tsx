ed6262feaaea75722398ad5183fd2b847ef5e2e6
masanori iha <masanori.iha@gmail.com>
2022-02-19 20:06:47 -0300

    iniciando instalação, configuração e ajustes iniciais no modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,25 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import logo from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
+interface HeaderProps {
+      onOpenNewTransactionModal: () => void;
+}
 
-export function Header(){
+export function Header({ onOpenNewTransactionModal }: HeaderProps){
+
  return (
        <header>
            <Container>
                   <Content>
                   <img src={logo} alt="dt money" />
-                  <button type="button">
+                  <button type="button" onClick={onOpenNewTransactionModal}>
                         Nova Transação
                   </button>
                   </Content>
+
+                  
            </Container>
        </header>
- )     
-}
+ ) }
